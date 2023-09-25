0b52092926d75f87ff021fa3d686e5ce166537f2
P-Ramos16 <p.ramos@ua.pt>
2023-09-25 17:50:47 +0100

    Cleaned some examples

--- Examples_cpp/classes-test.cpp
+++ Examples_cpp/classes-test.cpp
@@ -2,68 +2,78 @@
 #include <string>
 using namespace std;
 
-class Tank 
-{
+class Tank {
     private:
         int id;
     public:
 		string name;
 		string type;
-        Tank(){
-            cout << name << " is on wheels !!";
+
+        Tank() {
         }
 
-
+		void print() {
+            cout << name << " is on wheels !!";
+		}
 };
 
-int main()
 
-{
+int main() {
 
+	Tank tanks[5];
+    Tank nulltank; 
+	
+	nulltank.name = "null"; 
+	nulltank.type = "null";
 
-	Tank tanks[5] ;
-
-    Tank nulltank ; nulltank.name= "null"; nulltank.type="null";
-
-    for(int i = 0; i < sizeof(tanks)/sizeof(tanks[0]); i++)
-    {
+    for(int i = 0; i < sizeof(tanks) / sizeof(tanks[0]); i++) {
         tanks[i] = nulltank;
     }
 
-    Tank panzerIV; panzerIV.name = "panzerIV aug mk II"; panzerIV.type ="Medium";
+    Tank panzerIV; 
+	panzerIV.name = "panzerIV aug mk II"; 
+	panzerIV.type ="Medium";
 
-    Tank Panzerkampf ; Panzerkampf.name = "Panzerkampfwange ausch I"; Panzerkampf.type = "Heavy";
+    Tank Panzerkampf;
+	Panzerkampf.name = "Panzerkampfwange ausch I";
+	Panzerkampf.type = "Heavy";
 
-	tanks[0] = panzerIV; tanks[1] = Panzerkampf;
+	tanks[0] = panzerIV; 
+	tanks[1] = Panzerkampf;
 
 	string  asr;
 
-	while (true)
-	{
-		cout << "Want to add a new tank to the wiki ? \n [y/n] >>>";
+	while (true) {
+		cout << "Want to add a new tank to the wiki?\n [y/n] >>> ";
 		cin >> asr;
-		if (asr != "y"){
-		return 0;}
+
+		if (asr != "y") {
+			return 0;
+		}
+
 		Tank newTank;
-		cout << "\nInsert the name\n";
+		cout << "\nInsert the name\n   >>> ";
 		cin >>  newTank.name;
-		cout << "\nInsert the type (Light, Medium, Heavy, Super-Heavy, Amphibious, AA, Tank-Hunter)\n";
+		cout << "\nInsert the type (Light, Medium, Heavy, Super-Heavy, Amphibious, AA, Tank-Hunter)\n   >>> ";
 		cin >>  newTank.type;
 
-        for(int i = 0; i < sizeof(tanks)/sizeof(tanks[0]); i++)
-        {
-           if(tanks[i].name == nulltank.name){
-               tanks[i] = newTank;
-               break;
-           }
+        for(int i = 0; i < sizeof(tanks)/sizeof(tanks[0]); i++) {
+			if(tanks[i].name == nulltank.name) {
+				tanks[i] = newTank;
+				break;
+			}
         }
 
-		for(int i = 0; i < sizeof(tanks)/sizeof(tanks[0]); i++)
-		{
+		cout << "\n";
+		newTank.print();
+		cout << "\n";
+
+		for(int i = 0; i < sizeof(tanks)/sizeof(tanks[0]); i++) {
 		  cout << "\n" << i << " -- " << tanks[i].name;
 		} 
 
-	}	
+		cout << "\n\n";
+	}
 
 	return 0; 
 }
