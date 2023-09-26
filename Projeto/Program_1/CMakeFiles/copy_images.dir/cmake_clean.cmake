41a04324fadef9cdf938a57013694f8751a2e1d7
P-Ramos16 <p.ramos@ua.pt>
2023-09-26 18:52:53 +0100

    Finished exercice 1 of deliverable 1

---
+++ Projeto/Program_1/CMakeFiles/copy_images.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/copy_images.dir/copy_images.cpp.o"
+  "CMakeFiles/copy_images.dir/copy_images.cpp.o.d"
+  "copy_images"
+  "copy_images.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/copy_images.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Projeto/Program_1/copy_images.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <opencv4/opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main() {
+
+    //  Input and Output files
+    std::string inpFilename = "inpImg.jpg";
+    std::string outFilename = "outImg.jpg";
+
+    Vec3b value;
+
+    //  Load input image object, create the output with the same pixel width and height
+    Mat input = imread(inpFilename, IMREAD_COLOR);
+    Mat output(input.rows, input.cols, CV_8UC3, Scalar(10, 100, 150));
+    
+    std::cout << " Copying file " << inpFilename << " to " << outFilename << ".\n";
+
+    //  For every pixel column and row, copy to the blank image
+    for (int col = 0; col < input.cols; col++){
+        for (int row = 0; row < input.rows; row++){
+                value = input.at<Vec3b>(row,col);
+                output.at<Vec3b>(row, col) = value;
+        }
+    }
+
+    //  Write the blank image to the output file
+    imwrite(outFilename, output);
+
+    return 0;
+}
