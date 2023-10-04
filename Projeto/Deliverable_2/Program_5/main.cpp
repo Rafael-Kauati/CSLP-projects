573477cd5c9d8f713bf66de44fce3badf37015c8
P-Ramos16 <p.ramos@ua.pt>
2023-10-04 19:36:52 +0100

    Finished the main files for deliverable 2

---
+++ Projeto/Deliverable_2/Program_5/main.cpp
@@ -0,0 +1,77 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "../Program_4/BitStream.h"
+
+int main() {
+
+    cout << " ---------- Test program for BitStream ----------\n";
+    //  Initiate BitStream object
+    BitStream bitSt("input.bin", "output.bin");
+
+    cout << " ---------- Write One bit to a file ----------\n";
+    bitSt.writeOneFileBit(1);
+    bitSt.writeOneFileBit(0);
+    bitSt.writeOneFileBit(1);
+    bitSt.writeOneFileBit(1);
+    bitSt.writeOneFileBit(0);
+    bitSt.writeOneFileBit(0);
+    bitSt.writeOneFileBit(0);
+    bitSt.writeOneFileBit(1);
+
+    bitSt.writeOneFileBit(0);
+    bitSt.writeOneFileBit(0);
+    bitSt.writeOneFileBit(1);
+    bitSt.writeOneFileBit(0);
+    bitSt.writeOneFileBit(0);
+    bitSt.writeOneFileBit(1);
+    bitSt.writeOneFileBit(0);
+    bitSt.writeOneFileBit(0);
+
+    cout << " ---------- Read the bits in a file one by one ----------\n";
+    cout << " > read: [";
+    for (int i = 0; i < 8; i++) {
+        cout << bitSt.readOneFileBit(i);
+    }
+    cout << "] \n";
+
+    cout << " ---------- Read N bits in a file ----------\n";
+    //  Get all bits in the file
+    std::vector<int> bits = bitSt.readNFileBit(0,16);
+    
+    //  Print the bits
+    int counter = 0;
+    cout << " > read: [";
+    for(int i : bits) {
+
+        if (counter % 8 == 0 && counter != 0) {
+            cout << "] [";
+        }
+		std::cout << i;
+        counter++;
+    }    
+    cout << "] \n";
+
+    bitSt.writeNFileBit(bits);
+    bitSt.close();
+
+
+    BitStream charFiles("inputStrings.txt", "outputStrings.txt");
+
+    cout << " ---------- Write a Char to a file ----------\n";
+    charFiles.writeFileChar('P');
+    cout << " ---------- Write a String to a file ----------\n";
+    charFiles.writeFileString("Ramos");
+
+    
+    cout << " ---------- Read a Char in a file ----------\n";
+    cout << " > read Char: " << charFiles.readFileChar(0) << "\n";
+    cout << " > read Char: " << charFiles.readFileChar(8) << "\n";
+    cout << " > read Char: " << charFiles.readFileChar(16) << "\n";
+    cout << " > read Char: " << charFiles.readFileChar(24) << "\n";
+    cout << " > read Char: " << charFiles.readFileChar(32) << "\n";
+    cout << " ---------- Read a String in a file ----------\n";
+    cout << " > read String: " << charFiles.readFileString(0, 5) << "\n";
+    charFiles.close();
+}
