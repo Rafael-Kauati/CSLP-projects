ee05fb54e8b90f2d347f8be7d74b20bc3a315513
TK <kauati.toiti@gmail.com>
2023-10-09 17:27:51 +0100

    Deliverable 1, .3 working for some case, for details check the README.md of the deliverable

--- Golomb_Enconder.cpp
+++ Golomb_Enconder.cpp
@@ -1,75 +1,83 @@
-//
-// Created by tk on 08-10-2023.
-//
+#include <iostream>
+#include <vector>
+#include <cmath>
 
-#include "Golomb_Enconder.h"
+class GolombEnconder {
+public:
+    GolombEnconder(int m) : m(m) {}
 
+    // Codificar um número inteiro usando codificação de Golomb
+    std::vector<bool> encode(int num) {
+        int quotient = num / m;
+        int remainder = num % m;
 
-using namespace std;
+        std::cout << "quotient : " << quotient << std::endl;
+        std::cout << "remainder : " << remainder << std::endl;
 
-class Golomb_Enconder
-{
-    private:
-        int m, quotient, remainder;
-    std::vector<int> encodedBits; //
-    public:
+        std::vector<bool> encodedQuotient = unaryCode(quotient);
+        std::vector<bool> encodedRemainder = golombCode(remainder);
 
-        Golomb_Enconder(int param) : m(param) { std::cout << "Golomb parameter: " << m << std::endl; }
+        // Concatenar os bits codificados do quociente e do resto
+        std::vector<bool> encodedNumber = encodedQuotient;
+        encodedNumber.insert(encodedNumber.end(), encodedRemainder.begin(), encodedRemainder.end());
 
-        void encode(int value)
-        {
-            std::cout << "Int : "<<value<< std::endl;
-            quotient = value / m;
-            remainder = value % m;
-
-            std::cout << "Quotient: " << quotient << ", Remainder: " << remainder << std::endl;
-
-            for (int i = 0; i < quotient; ++i) {
-                encodedBits.push_back(1);
-            }
-            encodedBits.push_back(0);
-
-            //Use the remainder as ⌈log2​(m)⌉ bits
-            int numBits = static_cast<int>(std::log2(m));
-            for (int i = numBits - 1; i >= 0; --i) {
-                encodedBits.push_back((remainder >> i) & 1);
-            }
-
-            // Show the  bits encoded after each number of the non-negative sequence
-            std::cout << "Encoded bits: ";
-            for (int bit : encodedBits) {
-                std::cout << bit;
-            }
-            std::cout << std::endl;
-            std::cout << std::endl;
-
-            
-        }
-
-    void printEncodedBits() {
-
-        int nBit = 1;
-        for (bool bit : encodedBits) {
-            std::cout << "\n| "<< nBit << " : "<< bit;
-            nBit ++;
-        }
-        std::cout << std::endl;
+        return encodedNumber;
     }
+
+private:
+    std::vector<bool> golombCode(int num) {
+        int q = num / m;
+        int r = num % m;
+
+        // Calcular o número de bits necessários para representar m
+        int b = static_cast<int>(log2(m));
+        int threshold = (1 << b) - m;
+
+        std::vector<bool> binaryCodeRemainder;
+
+        // Se r < 2^b - m, então codifique r usando b bits
+        if (r < threshold) {
+            for (int i = b - 1; i >= 0; --i) {
+                binaryCodeRemainder.push_back((r >> i) & 1);
+            }
+
+        // Se r >= 2^b - m, codifique o número r + 2^b - m usando b + 1 bits
+        } else {
+            r += (1 << b) - m;
+            for (int i = b; i >= 0; --i) {
+                binaryCodeRemainder.push_back((r >> i) & 1);
+            }
+        }
+
+        return binaryCodeRemainder;
+    }
+
+    // Codificação unária de um número não negativo
+    std::vector<bool> unaryCode(int num) {
+        std::vector<bool> code;
+        for (int i = 0; i < num; ++i) {
+            code.push_back(1);
+        }
+        code.push_back(0); // Adicionar um 0 no final
+        return code;
+    }
+
+    int m; // Parâmetro m para codificação de Golomb
 };
 
 int main() {
-    int golombParameter = 11;
-    Golomb_Enconder encoder(golombParameter);
+    int m = 5;
+    GolombEnconder encoder(m);
 
-   // std::vector<int> values = {2, 5, 7, 3, 1, 8};
-   // for (int value : values) {
-        //encoder.encode(value);
-    //}
+    int num = 13;
+    std::vector<bool> encodedNumber = encoder.encode(num);
 
-    encoder.encode(37);
-
-    std::cout << "Encoded bits using Golomb coding: \n";
-    //encoder.printEncodedBits();
+    std::cout << "Número original: " << num << std::endl;
+    std::cout << "Bits codificados: ";
+    for (bool bit : encodedNumber) {
+        std::cout << bit;
+    }
+    std::cout << std::endl;
 
     return 0;
 }
--- README.md
+++ README.md
@@ -16,6 +16,30 @@ This project does not have test as it would not have passed them, so they were r
 ### Huffman enconding basic solution : 
 In golomb enconding (variation from huffman enconding)
 
+Cases that worked well : 
+
+n = 6 with m = 1
+quotient : 6
+remainder : 0
+Número original: 6
+Bits codificados: 11111100 
+
+n = 6 with m  = 4
+quotient : 1
+remainder : 2
+Número original: 6
+Bits codificados: 10010
+
+Cases that still doind bad (for some reason) :
+
+n = 13 with m  = 5
+
+
+quotient : 2
+remainder : 3
+Número original: 13
+Bits codificados: 110010 != 110011
+
 represent :
 
 n = 12 with m  = 5;
@@ -25,12 +49,17 @@ n -> q = 2(001) and r =2(10)
 n --> 00110
 
 
+
+
+
 represent :
 n = 13 with m = 5
 
 n -> q= 2 (001) r = 3(011)
 
 n --> 001110
+110011
+
 
 
 ## Other stuff
