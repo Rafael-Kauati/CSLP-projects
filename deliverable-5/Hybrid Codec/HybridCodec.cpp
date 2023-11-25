f19e54578bd21aefeb9f6a92d8510a31f6aecf37
Dan1M4d <daniel.madureira@ua.pt>
2023-11-25 17:21:34 +0000

    Added functions to encode video, frame and channel

--- deliverable-5/Hybrid Codec/HybridCodec.cpp
+++ deliverable-5/Hybrid Codec/HybridCodec.cpp
@@ -20,22 +20,23 @@ private:
 public:
     HybridCodec(/* args */);
     ~HybridCodec();
-    encode();
+    encodeVideo();
+    encodeFrame();
+    encodeChannel();
+
     decode();
     close();
     Frame_Predicter p;
-
 };
 
 HybridCodec::HybridCodec(string inputfile, string outputfile, int blockSize = 8, int searchSize = 16, int frequency = 100)
-: p(inputfile, outputfile)
+    : p(inputfile, outputfile)
 {
     this->inputfile = inputfile;
     this->outputfile = outputfile;
     this->BLOCK_SIZE = blockSize;
     this->SEARCH_SIZE = searchSize;
     this->frequency = frequency;
-
 }
 
 HybridCodec::~HybridCodec()
@@ -43,12 +44,11 @@ HybridCodec::~HybridCodec()
 }
 
 void writeParams(int newmParam, int newxFrameSize, int newyFrameSize,
-                 int newfileType, int newnumFrames=1, int newfps=1)
+                 int newfileType, int newnumFrames = 1, int newfps = 1)
 
 {
-    p.writeParams(int newmParam, int newxFrameSize, int newyFrameSize, int newfileType, int newnumFrames=1, int newfps=1);
+    p.writeParams(int newmParam, int newxFrameSize, int newyFrameSize, int newfileType, int newnumFrames = 1, int newfps = 1);
 }
-
 
 void readParams()
 {
@@ -66,9 +66,9 @@ void writeVideo(cv::VideoCapture video)
 }
 
 void close()
-    {
-        p.closeStreams();
-    }
+{
+    p.closeStreams();
+}
 
 void decode()
 {
@@ -105,7 +105,7 @@ void decode()
             Mat decodedFrame;
             merge(channels, decodedFrame);
 
-            //outputVideo.write(decodedFrame);
+            // outputVideo.write(decodedFrame);
 
             prevFrame = decodedFrame;
             count++;
@@ -116,7 +116,7 @@ void decode()
     }
 }
 
-void encode()
+/* void encode()
 {
     int count = 0;
 
@@ -134,7 +134,7 @@ void encode()
         {
             // if is the frequency-th frame, encode the frame with intra-frame coding
             Frame_Predicter fp;
-            
+
             fp.writeFrameColour(prevFrame, outputfile);
 
             // encode the frame
@@ -185,6 +185,7 @@ void encode()
 
                         // encode the diff and the deslocation vector
                         g.encodeBlock(diff);
+                        // write deslocation vector to file
                         g.encode(x - x_0);
                         g.encode(y - y_0);
                     }
@@ -195,5 +196,100 @@ void encode()
         // update the previous frame
         prevFrame = frame;
         count++;
+    }
+} */
+
+void encodeVideo() {
+    int count = 0;
+    Mat frame;
+    Mat prevFrame;
+
+    // check if the video can be opened
+    VideoCapture cap(inputfile);
+    if (!cap.isOpened())
+    {
+        cout << "Error opening video stream or file" << endl;
+        return;
+    }
+
+    // write the parameters to the file
+    writeParams(g.m, frame.cols, frame.rows, 1, 1, 1);
+
+    // loop through the video
+
+    while (!frame.empty())
+    {
+        //read the frame
+        cap >> frame;
+
+        if (count % frequency == 0)
+        {
+            // if is the frequency-th frame, encode the frame with intra-frame coding
+            Frame_Predicter fp;
+
+            fp.writeFrameColour(frame, outputfile);
+
+            // encode the frame
+            g.encodeBlock(frame);
+        }
+        else
+        {
+            // if is not the frequency-th frame, encode the frame with inter-frame coding
+            encodeFrame(frame, prevFrame);
+        }
+
+        prevFrame = frame;
+        count++;
+
+    }
+}
+
+void encodeFrame(Mat frame, Mat prevFrame)
+{
+    // split the frame into channels
+    vector<Mat> channels;
+    split(frame, channels);
+
+    // split the previous frame into channels
+    vector<Mat> prevChannels;
+    split(prevFrame, prevChannels);
+
+    // encode each channel
+    encodeChannel(channels[0], prevChannels[0]);
+    encodeChannel(channels[1], prevChannels[1]);
+    encodeChannel(channels[2], prevChannels[2]);
+}
+
+void encodeChannel(Mat channel, Mat prevChannel)
+{
+    // get the height and width of the channe
+    int height = channel.rows;
+    int width = channel.cols;
+
+    // loop through the channel in blocks
+
+    for (int y = 0; y < height; y += BLOCK_SIZE)
+    {
+        for (int x = 0; x < width; x += BLOCK_SIZE)
+        {
+            Mat block = channel(Rect(x, y, BLOCK_SIZE, BLOCK_SIZE));
+            // get the best block
+            vector<int> bestBlock = blockSearch.findBestBlock(prevFrame, block, x, y);
+            // get the best block's coordinates
+            int x_0 = bestBlock[0];
+            int y_0 = bestBlock[1];
+            // get the best block
+            Mat bestBlock = prevFrame(Rect(x_0, y_0, BLOCK_SIZE, BLOCK_SIZE));
+
+            // get the diff between the best block and the current block
+            Mat diff;
+            subtract(block, bestBlock, diff);
+
+            // encode the diff and the deslocation vector
+            g.encodeBlock(diff);
+            // write deslocation vector to file
+            g.encode(x - x_0);
+            g.encode(y - y_0);
+        }
     }
 }
