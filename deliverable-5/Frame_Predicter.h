2bb92fb5604c0ec8ab8f2af5694f8880fa5eee64
P-Ramos16 <p.ramos@ua.pt>
2023-11-25 15:52:17 +0000

    Updated write and read params to allow for hybrid params

--- deliverable-5/Frame_Predicter.h
+++ deliverable-5/Frame_Predicter.h
@@ -3,8 +3,8 @@
 #include <string>
 #include <vector>
 #include <opencv4/opencv2/opencv.hpp>
-#include "Golomb_Encoder.hpp"
-#include "Golomb_Decoder.hpp"
+#include "./Golomb/Golomb_Encoder.hpp"
+#include "./Golomb/Golomb_Decoder.hpp"
 
 using namespace std;
 
@@ -20,6 +20,10 @@ private:
     int fileType;
     int numFrames;
     int fps;
+    int block_size;
+    int search_area;
+    int intraframe_period;
+
 
     //  Public methods
     public:
@@ -31,19 +35,26 @@ private:
             yFrameSize = 0;
             fileType = 0;
             fps = 0;
+            block_size = 0;
+            search_area = 0;
+            intraframe_period = 0;
         }
 
         void closeStreams() {
             encoder.closeStreams();
         }
 
-    void writeParams(int newmParam, int newxFrameSize, int newyFrameSize, int newfileType, int newnumFrames=1, int newfps=1) {
+    void writeParams(int newmParam, int newxFrameSize, int newyFrameSize, int newfileType, int newnumFrames=1, int newfps=1, int newblock_size=1, int newsearch_area=1, int newintraframe_period=1) {
         mParam = newmParam;
         xFrameSize = newxFrameSize;
         yFrameSize = newyFrameSize;
         fileType = newfileType;
         numFrames = newnumFrames;
         fps = newfps;
+        block_size = newblock_size;
+        search_area = newsearch_area;
+        intraframe_period = newintraframe_period;
+
         //  Write the m parameter with 1 byte (max: 255)
         encoder.writeInt(newmParam, 1);
         //  Write the x and y frame sizes with 2 bytes each (max: 65,535)
@@ -55,6 +66,12 @@ private:
         encoder.writeInt(newnumFrames, 4);
         //  Write the FPS of the video with 1 byte (max: 255)
         encoder.writeInt(newfps, 1);
+        //  Write the block size of the video with 2 bytes (max: 65,535)
+        encoder.writeInt(newblock_size, 2);
+        //  Write the search area size of the video 2 bytes (max: 65,535)
+        encoder.writeInt(newsearch_area, 2);
+        //  Write the period between intraframes of the video with 1 byte (max: 255)
+        encoder.writeInt(newintraframe_period, 1);
 
         encoder.setMParam(newmParam);
     }
@@ -72,6 +89,12 @@ private:
         this->numFrames = decoder.readInt(4);
         //  Read the FPS of the video with 1 byte (max: 255)
         this->fps = decoder.readInt(1);
+        //  Read the block size of the video with 2 bytes (max: 65,535)
+        this->block_size = decoder.readInt(2);
+        //  Read the search area size of the video with 2 bytes (max: 65,535)
+        this->search_area = decoder.readInt(2);
+        //  Read the period between intraframes of the video with 1 byte (max: 255)
+        this->intraframe_period = decoder.readInt(1);
 
         decoder.setMParam(this->mParam);
     }
