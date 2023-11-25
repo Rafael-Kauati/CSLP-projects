a9bed62b76ac1efddf67485362db236a00a57257
Dan1M4d <daniel.madureira@ua.pt>
2023-11-25 15:18:45 +0000

    Basic structure for frequecy intra-frame encoding on hybrid codec

--- deliverable-5/Hybrid Codec/HybridCodec.cpp
+++ deliverable-5/Hybrid Codec/HybridCodec.cpp
@@ -4,6 +4,7 @@
 #include <opencv2/videoio.hpp>
 #include <cmath>
 #include "Golomb.hpp"
+#include "Frame_Predicter.h"
 
 using namespace std;
 using namespace cv;
@@ -95,50 +96,64 @@ void encode()
     // loop through the video
     while (!prevFrame.empty())
     {
-        cap >> frame;
-        // if the frame is empty, break immediately
-        if (frame.empty())
+        if (count % frequency == 0)
         {
-            break;
+            // if is the frequency-th frame, encode the frame with intra-frame coding
+            Frame_Predicter fp;
+            
+            fp.writeFrameColour(prevFrame, outputfile);
+
+            // encode the frame
+            g.encodeBlock(prevFrame);
         }
-        // split the frame into channels
-        split(prevFrame, channels);
-        split(frame, channels);
-
-        BlockSearch blockSearch(BLOCK_SIZE, SEARCH_SIZE);
-
-        // loop through the channels
-        for (int i = 0; i < channels.size(); i++)
+        else
         {
-            // get the current channel
-            Mat channel = channels[i];
-            // get the height and width of the channel
-            int height = channel.rows;
-            int width = channel.cols;
 
-            // loop through the channel in blocks
-
-            for (int y = 0; y < height; y += BLOCK_SIZE)
+            cap >> frame;
+            // if the frame is empty, break immediately
+            if (frame.empty())
             {
-                for (int x = 0; x < width; x += BLOCK_SIZE)
+                break;
+            }
+            // split the frame into channels
+            split(prevFrame, channels);
+            split(frame, channels);
+
+            BlockSearch blockSearch(BLOCK_SIZE, SEARCH_SIZE);
+
+            // loop through the channels
+            for (int i = 0; i < channels.size(); i++)
+            {
+                // get the current channel
+                Mat channel = channels[i];
+                // get the height and width of the channel
+                int height = channel.rows;
+                int width = channel.cols;
+
+                // loop through the channel in blocks
+
+                for (int y = 0; y < height; y += BLOCK_SIZE)
                 {
-                    Mat block = channel(Rect(x, y, BLOCK_SIZE, BLOCK_SIZE));
-                    // get the best block
-                    vector<int> bestBlock = blockSearch.findBestBlock(prevFrame, block, x, y);
-                    // get the best block's coordinates
-                    int x_0 = bestBlock[0];
-                    int y_0 = bestBlock[1];
-                    // get the best block
-                    Mat bestBlock = prevFrame(Rect(x_0, y_0, BLOCK_SIZE, BLOCK_SIZE));
+                    for (int x = 0; x < width; x += BLOCK_SIZE)
+                    {
+                        Mat block = channel(Rect(x, y, BLOCK_SIZE, BLOCK_SIZE));
+                        // get the best block
+                        vector<int> bestBlock = blockSearch.findBestBlock(prevFrame, block, x, y);
+                        // get the best block's coordinates
+                        int x_0 = bestBlock[0];
+                        int y_0 = bestBlock[1];
+                        // get the best block
+                        Mat bestBlock = prevFrame(Rect(x_0, y_0, BLOCK_SIZE, BLOCK_SIZE));
 
-                    // get the diff between the best block and the current block
-                    Mat diff;
-                    subtract(block, bestBlock, diff);
+                        // get the diff between the best block and the current block
+                        Mat diff;
+                        subtract(block, bestBlock, diff);
 
-                    // encode the diff and the deslocation vector
-                    g.encodeBlock(diff);
-                    g.encode(x - x_0);
-                    g.encode(y - y_0);
+                        // encode the diff and the deslocation vector
+                        g.encodeBlock(diff);
+                        g.encode(x - x_0);
+                        g.encode(y - y_0);
+                    }
                 }
             }
         }
