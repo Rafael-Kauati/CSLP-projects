df464151f8373f577b29c46b1b96f8f1f22cc07b
P-Ramos16 <p.ramos@ua.pt>
2023-11-26 22:11:11 +0000

    Added deliverable 5's README and screenshots

---
+++ Projeto/Deliverable_5/CMakeFiles/unit_tests.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/frostywolf/Documents/GitReps/CSLP-projects/Projeto/Deliverable_5/Golomb/Golomb.cpp" "CMakeFiles/unit_tests.dir/Golomb/Golomb.cpp.o" "gcc" "CMakeFiles/unit_tests.dir/Golomb/Golomb.cpp.o.d"
+  "/home/frostywolf/Documents/GitReps/CSLP-projects/Projeto/Deliverable_5/Golomb/Golomb_Decoder.cpp" "CMakeFiles/unit_tests.dir/Golomb/Golomb_Decoder.cpp.o" "gcc" "CMakeFiles/unit_tests.dir/Golomb/Golomb_Decoder.cpp.o.d"
+  "/home/frostywolf/Documents/GitReps/CSLP-projects/Projeto/Deliverable_5/Golomb/Golomb_Encoder.cpp" "CMakeFiles/unit_tests.dir/Golomb/Golomb_Encoder.cpp.o" "gcc" "CMakeFiles/unit_tests.dir/Golomb/Golomb_Encoder.cpp.o.d"
+  "/home/frostywolf/Documents/GitReps/CSLP-projects/Projeto/Deliverable_5/HybridCodec/BlockSearch.cpp" "CMakeFiles/unit_tests.dir/HybridCodec/BlockSearch.cpp.o" "gcc" "CMakeFiles/unit_tests.dir/HybridCodec/BlockSearch.cpp.o.d"
+  "/home/frostywolf/Documents/GitReps/CSLP-projects/Projeto/Deliverable_5/HybridCodec/HybridCodec.cpp" "CMakeFiles/unit_tests.dir/HybridCodec/HybridCodec.cpp.o" "gcc" "CMakeFiles/unit_tests.dir/HybridCodec/HybridCodec.cpp.o.d"
+  "/home/frostywolf/Documents/GitReps/CSLP-projects/Projeto/Deliverable_5/unit_tests.cpp" "CMakeFiles/unit_tests.dir/unit_tests.cpp.o" "gcc" "CMakeFiles/unit_tests.dir/unit_tests.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- Projeto/deliverable-5/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/frostywolf/Documents/GitReps/CSLP-projects/Projeto/deliverable-5
-# Build directory: /home/frostywolf/Documents/GitReps/CSLP-projects/Projeto/deliverable-5
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/home/frostywolf/Documents/GitReps/CSLP-projects/Projeto/deliverable-5/unit_tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/frostywolf/Documents/GitReps/CSLP-projects/Projeto/deliverable-5/CMakeLists.txt;23;add_test;/home/frostywolf/Documents/GitReps/CSLP-projects/Projeto/deliverable-5/CMakeLists.txt;0;")
