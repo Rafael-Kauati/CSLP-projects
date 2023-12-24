f844099b09b6ebb1f80627b72d8fc43f0e909d71
P-Ramos16 <p.ramos@ua.pt>
2023-12-24 18:22:00 +0000

    Cleaned the file tree

---
+++ Tests_Read_YUV/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/frostywolf/Documents/GitReps/CSLP-projects/Projeto/Tests_Read_YUV
+# Build directory: /home/frostywolf/Documents/GitReps/CSLP-projects/Projeto/Tests_Read_YUV
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(main "/home/frostywolf/Documents/GitReps/CSLP-projects/Projeto/Tests_Read_YUV/main")
+set_tests_properties(main PROPERTIES  _BACKTRACE_TRIPLES "/home/frostywolf/Documents/GitReps/CSLP-projects/Projeto/Tests_Read_YUV/CMakeLists.txt;14;add_test;/home/frostywolf/Documents/GitReps/CSLP-projects/Projeto/Tests_Read_YUV/CMakeLists.txt;0;")
